ff289a2328a7664717e50f96e80487fbdc59f8f8
phizeroth <phi.zeroth@gmail.com>
2017-04-16 13:42:25 -0400

    eliminate multiple API calls

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
 var units = ['C', 'F'];
 var unitIndex = 1;    //set default to Fahrenheit
 var loc = '';
+var temperature = {};
 
-function foo(lat, long) {
+function initWeather(lat, long) {
   loc = lat + ',' + long;
   loadWeather(loc, units[unitIndex]);
 }
 
-function loadWeather(loc, unit) {
+function loadWeather(loc) {
     console.log(loc);
     $.simpleWeather({
-      unit: unit,
       location: loc,
+      unit: 'C',
       success: function(weather) {
         $('#spinner').css('display', 'none');
-        $('.content').css('display', 'inherit');
-        $('.container').css('display', 'block');
+        $('.content').css('display', 'flex');
+        $('.container').css('display', 'inline');
         $('#city').html(weather.city);
         $('#region').html(weather.region + ', ' + weather.country);
         $('#date').html(moment().format('dddd, MMMM D, YYYY'));
         $('.icon').attr('data-icon', 'N');
-        $('#temperature').html(weather.temp);
-        $('#unit').html('°' + this.unit);
-        $('#hi').html('HI ' + weather.high);
-        $('#lo').html('LO ' + weather.low);
         $('#weather-title').html(weather.currently);
         $('#icon').addClass('wi-yahoo-' + weather.code)
         console.log(weather.code);
+        temperature = {
+          C: {
+            temp: weather.temp,
+            hi: weather.high,
+            lo: weather.low
+          },
+          F: {
+            temp: weather.alt.temp,
+            hi: weather.alt.high,
+            lo: weather.alt.low
+          }
+        };
+        updateTemps('F', temperature);
       }
     });
+  }
+
+  function updateTemps(unit, temps) {
+    let t = temps[unit];
+    console.log(temps);
+    $('#temperature').html(t.temp);
+    $('#unit').html('°' + unit);
+    $('#hi').html(t.hi);
+    $('#lo').html(t.lo);
   }
 
 $(document).ready(function() {
 
   navigator.geolocation.getCurrentPosition(function(position) {
-    foo(position.coords.latitude, position.coords.longitude);
+    initWeather(position.coords.latitude, position.coords.longitude);
   });
 
   $('#unit').on('click', function() {
     unitIndex ^= 1;   //toggle units
-    loadWeather(loc, units[unitIndex]);
+    updateTemps(units[unitIndex], temperature);
+    // loadWeather(loc, units[unitIndex]);
   });
 
 });
